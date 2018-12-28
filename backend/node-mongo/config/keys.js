98985ffed6c5212cc2aa8b233e86ffd23f479eda
silent-sea1119 <sandragonzales56t@gmail.com>
2018-12-28 10:52:51 -0300

    removing social network project

--- backend/node-mongo/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // this is a test database just to show data populated.
-  // change the URI to put yours
-  mongoURI: "mongodb://social-network-mern-user:TkkP4x9w9gFDNE48@ds139655.mlab.com:39655/social-network-mern",
-  secretOrKey: "secret"
-};
