4398f319e002d9af36e6e18e652c71bcbe0de95f
silent-sea1119 <sandragonzales56t@gmail.com>
2018-12-28 15:40:57 -0300

    Creating components folder
    ... and importing Navbar component into the App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>We are running</h1>
+        <NavBar/>
       </div>
     );
   }
