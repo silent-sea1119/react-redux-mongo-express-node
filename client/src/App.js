4941118c0f4d049a93ebb5cbe7fdd53a830af427
silent-sea1119 <sandragonzales56t@gmail.com>
2018-12-28 17:07:38 -0300

    Creating Footer and Landing components
    ... and placing them into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar/>
+        <Landing/>
+        <Footer/>
       </div>
     );
   }
