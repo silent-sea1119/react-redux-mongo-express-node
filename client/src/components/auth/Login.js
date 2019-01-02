c595938ceb22ef3f5a19ccdd4f7019b150743390
silent-sea1119 <sandragonzales56t@gmail.com>
2019-01-02 15:10:22 -0300

    User registration with component state
    - no redux yet

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -31,7 +31,7 @@ class Login extends Component {
                             <div className="col-md-8 m-auto">
                                 <h1 className="display-4 text-center">Log In</h1>
                                 <p className="lead text-center">Sign in to your DevConnector account</p>
-                                <form onSubmit={this.onSubmit}>
+                                <form onSubmit={this.onSubmit}> 
                                     <div className="form-group">
                                         <input
                                             type="email"
