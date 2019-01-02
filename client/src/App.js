53f916b086ae977604953040f9a2eb20022a9b08
silent-sea1119 <sandragonzales56t@gmail.com>
2019-01-02 10:46:09 -0300

    Using react-router-dom to create a route
    ... for the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -8,11 +8,13 @@ import Landing from './components/layout/Landing';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <NavBar/>
-        <Landing/>
+        <Route exact path="/" component={Landing} />
         <Footer/>
       </div>
+      </Router>
     );
   }
 }
