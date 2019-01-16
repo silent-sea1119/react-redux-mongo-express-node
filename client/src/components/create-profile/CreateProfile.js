b6e6017e25da36205dfedc915f7c4abe875d72f9
silent-sea1119 <sandragonzales56t@gmail.com>
2019-01-15 23:21:02 -0300

    Creating form fields components

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -52,4 +55,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps)(CreateProfile);
