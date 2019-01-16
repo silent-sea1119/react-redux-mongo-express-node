14404e2924d26513f59361b54e2e5e7532340c0b
silent-sea1119 <sandragonzales56t@gmail.com>
2019-01-15 22:41:48 -0300

    Creating CreateProfile Component & Route

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from './components/create-profile/CreateProfile';
 import Login from "./components/auth/Login";
 import "./App.css";
 
@@ -53,6 +54,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
