c5d9bb16c65b4a518b66f08c51b3ef998924ca8b
silent-sea1119 <sandragonzales56t@gmail.com>
2019-02-11 20:48:46 -0300

    Returning profiles from reducer
    Fixing the object that gets returned from
    the profileReducer.

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -27,7 +27,7 @@ export default function (state = initialState, action) {
     case GET_PROFILES:
       return {
         ...state,
-        profile: action.payload,
+        profiles: action.payload,
         loading: false
       }
     case CLEAR_CURRENT_PROFILE:
