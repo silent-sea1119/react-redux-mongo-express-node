69bfc5cebb6db758c69f7c0bfbe2388dc0e843ce
silent-sea1119 <sandragonzales56t@gmail.com>
2019-02-13 20:56:58 -0300

    Profile Header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,75 +1,72 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import ProfileHeader from './ProfileHeader';
-import ProfileAbout from './ProfileAbout';
-import ProfileCreds from './ProfileCreds';
-import ProfileGithub from './ProfileGithub';
-import Spinner from '../common/Spinner';
-import { getProfileByHandle } from '../../actions/profileActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+import Spinner from "../common/Spinner";
+import { getProfileByHandle } from "../../actions/profileActions";
 
 class Profile extends Component {
-
-    componentDidMount() {
-        // when we click into View Profile button that will load a Profile, we need to get the handle
-        // from url. We can do this using react router
-        if (this.props.match.params.handle) {
-            this.props.getProfileByHandle(this.props.match.params.handle);
-        }
+  componentDidMount() {
+    // when we click into View Profile button that will load a Profile, we need to get the handle
+    // from url. We can do this using react router
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
     }
+  }
 
+  render() {
+    const { profile, loading } = this.props.profile;
 
-    render() {
+    let profileContent;
 
-        const { profile, loading } = this.props.profile;
-
-        let profileContent;
-
-        if (profile === null || loading) {
-            profileContent = <Spinner />
-        } else {
-            profileContent = (
-                <div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <Link to="/profiles" className="btn btn-light mb-3 float-left">
-                                Back To Profiles
-                            </Link>
-                            <div className="col-md-6"></div>
-                        </div>
-                    </div>
-                    {/* passing a property to a component in React is so easy! */}
-                    <ProfileHeader profile={profile} />
-                    <ProfileAbout />
-                    <ProfileCreds />
-                    <ProfileGithub />
-                </div>
-            )
-        }
-
-        return (
-            <div className="profile">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            {profileContent}
-                        </div>
-                    </div>
-                </div>
-
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+              <div className="col-md-6" />
             </div>
-        )
+          </div>
+          {/* passing a property to a component in React is so easy! */}
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      );
     }
+
+    return (
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Profile.propTypes = {
-    profile: PropTypes.object.isRequired,
-    getProfileByHandle: PropTypes.func.isRequired
-}
+  profile: PropTypes.object.isRequired,
+  getProfileByHandle: PropTypes.func.isRequired
+};
 
 const mapStateToProps = state => ({
-    profile: state.profile
+  profile: state.profile
 });
 
-export default connect(mapStateToProps, { getProfileByHandle })(Profile);
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
