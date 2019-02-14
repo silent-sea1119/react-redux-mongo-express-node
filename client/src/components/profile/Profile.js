504893a62a1fdb174d7f536f0fd9b89a47f11694
silent-sea1119 <sandragonzales56t@gmail.com>
2019-02-14 20:28:09 -0300

    fixing username undefined
    Fixed username undefined into ProfileGithub
    component.
    
    The problem was that we were trying to receive
    a property username from the parent component
    (Profile), but that was not being sent.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -46,7 +46,7 @@ class Profile extends Component {
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
           <ProfileCreds education={profile.education} experience={profile.experience} />
-          {profile.githubusername ? <ProfileGithub /> : null}
+          {profile.githubusername ? <ProfileGithub username={profile.githubusername} /> : null}
         </div>
       );
     }
