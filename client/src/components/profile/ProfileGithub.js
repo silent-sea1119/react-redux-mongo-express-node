194e72a4b3c0820d4daa6e6efdc0e6548cad739e
silent-sea1119 <sandragonzales56t@gmail.com>
2019-02-15 16:22:55 -0300

    solving an issue while trying to fetch repos from github with an unauthorized profile

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -24,7 +24,9 @@ class ProfileGithub extends Component {
     )
       .then(res => res.json())
       .then(data => {
-        if (this.refs.myRef) {
+        if (data.message === "Not Found") {
+          this.setState({ repos: [] });
+        } else if (this.refs.myRef) {
           this.setState({ repos: data });
         }
       })
@@ -33,26 +35,36 @@ class ProfileGithub extends Component {
 
   render() {
     const { repos } = this.state;
-
-    const repoItems = repos.map(repo => (
-      <div key={repo.id} className="card card-body mb-2">
-        <div className="row">
-          <div className="col-md-6">
-            <h4>
-              <Link to={repo.html_url} className="text-info" target="_blank">
-                {repo.name}
-              </Link>
-            </h4>
-            <p>{repo.description}</p>
-          </div>
-          <div className="col-md-6">
-            <span className="badge badge-info mr-1">Stars: {repo.stargazers_count}</span>
-            <span className="badge badge-secondary mr-1">Watchers: {repo.watchers_count}</span>
-            <span className="badge badge-success">Forks: {repo.forks_count}</span>
+    let repoItems;
+    if (repos.length === 0) {
+      repoItems = "This profile is not authorized to fetch github repos.";
+    } else {
+      repoItems = repos.map(repo => (
+        <div key={repo.id} className="card card-body mb-2">
+          <div className="row">
+            <div className="col-md-6">
+              <h4>
+                <Link to={repo.html_url} className="text-info" target="_blank">
+                  {repo.name}
+                </Link>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div className="col-md-6">
+              <span className="badge badge-info mr-1">
+                Stars: {repo.stargazers_count}
+              </span>
+              <span className="badge badge-secondary mr-1">
+                Watchers: {repo.watchers_count}
+              </span>
+              <span className="badge badge-success">
+                Forks: {repo.forks_count}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ));
+    }
 
     return (
       <div ref="myRef">
