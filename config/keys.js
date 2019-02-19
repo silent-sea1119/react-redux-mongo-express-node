db4b5ec584bd2e08169301bdcba154f1dbe64aea
silent-sea1119 <sandragonzales56t@gmail.com>
2019-02-18 21:02:11 -0300

    securing our keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  // this is a test database just to show data populated.
-  // change the URI to put yours
-  mongoURI: "mongodb://social-network-mern-user:TkkP4x9w9gFDNE48@ds139655.mlab.com:39655/social-network-mern",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
